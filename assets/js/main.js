f3fd4418df20a95127db5801333413513ce3a3b7
dikobrazik <ilnar.khusaenov@gmail.com>
2018-12-23 08:45:26 +0300

    v0.5.2

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+      document.getElementsByClassName('container')[0].style.marginTop = document.getElementsByClassName('navbar-expand-md')[0].offsetHeight+'px';
+  });
