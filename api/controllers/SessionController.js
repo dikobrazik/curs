db6086f98d5a5cad40a625e2010f4897af27e18d
dikobraz <ilnar.khusaenov@gmail.com>
2019-01-29 16:48:38 +0300

    0.5.9

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -7,30 +7,42 @@
 var crypto = require('crypto');
 
 module.exports = {
-    create: function (req, res) {
+    create: async function (req, res) {
         var username = req.param('username'),
             password = req.param('password');
-        console.log(req.headers)
         if (!username || !password) {
+            if(req.headers['user-agent'] == 'mobile-app') return res.status(204).send('No Content');
             return res.redirect('/login');
         };
         if (req.session.auth) {
+            if(req.headers['user-agent'] == 'mobile-app') return res.status(208).send('Already Reported');
             return res.redirect('/groups');
         };
-        User.find({username:username}).exec(function (err, user) {
-            if (!user || err) return res.redirect('/login');
-            var hash = crypto.createHash('sha256');
-            hash.update(password);
-            var hHash = hash.digest('hex');
-            if (hHash == user[0].password) {
-                req.session.auth = true;
-                req.session.User = user;
-                console.log(req.session);
-                if (req.session.User[0].admin) {
-                    return res.redirect('/groups');
-                };
-            }else{res.redirect('/login');}
-        });
+        try{
+            let user = await User.findOne({username:username});
+            if (!user) {
+                if(req.headers['user-agent'] == 'mobile-app') return res.status(404).send('Not Found');
+                return res.redirect('/login');
+            }else{
+                var hash = crypto.createHash('sha256');
+                hash.update(password);
+                var hHash = hash.digest('hex');
+                if (hHash == user.password) {
+                    req.session.auth = true;
+                    req.session.User = user;
+                    console.log(req.session);
+                    if(req.headers['user-agent'] == 'mobile-app') res.send(req.session);
+                    if (req.session.User.admin) {
+                        return res.redirect('/groups');
+                    }
+                }else{
+                    if(req.headers['user-agent'] == 'mobile-app') res.status(400).send('Password is wrong');
+                    return res.redirect('/login');
+                }
+            }
+        }catch(err){
+            console.log(err)
+        }
     },
     destroy: function (req, res) {
         console.log(req.session.User);
