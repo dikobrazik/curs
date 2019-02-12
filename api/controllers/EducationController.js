c892a5eb07bdbdc44d626f046269df8dac66c9c7
dikobraz <ilnar.khusaenov@gmail.com>
2019-02-12 22:40:16 +0300

    0.6.2

--- api/controllers/EducationController.js
+++ api/controllers/EducationController.js
@@ -7,8 +7,8 @@
 
 module.exports = {
     update: async function (req, res) {
-        console.log(req.param('data'));
-        var Id = await Education.findOrCreate({
+        //return res.ok()
+        /*var Id = await Education.findOrCreate({
             gNum:req.param('data').gNum,
         },{
             gNum : req.param('data').gNum,
@@ -17,15 +17,35 @@ module.exports = {
         var elem = {
             content:req.param('data').content,
         };
+        let groupSubjects = JSON.parse(elem.content);
+        for(let i = 0; i < groupSubjects.length; i++){
+            for(let j = 0; j < groupSubjects[i].length; j++){
+                if(groupSubjects[i][j]){
+                    let user = await User.findOne({
+                        name:groupSubjects[i][j].username.split(' ')[0],
+                        surname:groupSubjects[i][j].username.split(' ')[1]
+                    })*/
+        let data = req.param('data');
+        Subject.create({
+            gNum:data.gNum,
+            userId:data.userId,
+            day:data.day,
+            number:data.number,
+        }).exec((err)=>{
+            if(err) return res.sendStatus(500)
+            return res.sendStatus(200)
+        })/*
+                }
+            }
+        }
         Education.update(Id.id,elem).exec(function(err){
             if(err) return res.sendStatus(500);
             return res.ok();
-        });
-            
+        });*/
     },
     //расписание определенной группы
     timetable:function(req, res){
-        if(req.headers['user-agent'] === 'mobile-app'){
+        /*if(req.headers['user-agent'] === 'mobile-app'){
             return res.send(req);
         }
         Education
@@ -34,11 +54,17 @@ module.exports = {
                 if (err) return res.send(500);
                 if (!data) return res.send(204);
                 return res.send(data.content);
-            });
+            });*/
+        Subject
+            .find({gNum:req.param('index')})
+            .exec((err, data)=>{
+                if(err) return res.sendStatus(500)
+                return res.send(data)
+            })
     },
     //группы которым преподает данный препод
     teacherGroups:function(req, res){
-        if(req.headers['user-agent'] === 'mobile-app'){
+        /*if(req.headers['user-agent'] === 'mobile-app'){
             //return res.send(req);
         }
         Education
@@ -59,7 +85,11 @@ module.exports = {
                 },err=>{
                     console.log(err)
                 });
-            });
+            });*/
+        Subject.find({userId:req.param('id')}).exec((err, data)=>{
+            if(err) return res.sendStatus(500)
+            return res.send(data)
+        })
       let _getUser = async () => {
         return await User.findOne({id: req.param('id')});
       }
