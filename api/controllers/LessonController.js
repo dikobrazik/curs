522521f53f8a74e0bb48a148670f38de6172b9cd
dikobraz <ilnar.khusaenov@gmail.com>
2019-05-13 10:32:32 +0300

    0.6.5.1

--- api/controllers/LessonController.js
+++ api/controllers/LessonController.js
@@ -1,8 +1,6 @@
 /**
  * LessonController
  *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
 module.exports = {
